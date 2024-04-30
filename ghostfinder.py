e6bf085bf9261f48eb0293bf94eee79407962312
John Peter <106468893+johncybersec1@users.noreply.github.com>
2024-04-30 12:05:29 +0200

    Updated ghostfinder.py
    added keyboard interrupt.

--- ghostfinder.py
+++ ghostfinder.py
@@ -2,6 +2,10 @@ import ping3
 
 #Author :John Peter
 #Tool: Ghost finder
+import ping3
+import sys
+#Author :John Peter
+#Tool: Ghost finder
 print("GHOST FINDER")
 print()
 print("Discover available hosts in your network with this tool.")
@@ -17,18 +21,24 @@ counter = 1
 active_hosts = []
 print("This might take a while :(")
 print("Searching...")
-while counter <=255:
-    octets = net_mask.split(".")
-    last_octet = str(int(octets[-1]) + counter)
-    current_ip = ".".join(octets[:-1] + [last_octet])
-    response_time = ping3.ping(current_ip)
-    if response_time is not False:
-        active_hosts.append(current_ip)
-        counter += 1
-    else:
-        counter += 1
+try:
+    while counter <=255:
+        octets = net_mask.split(".")
+        last_octet = str(int(octets[-1]) + counter)
+        current_ip = ".".join(octets[:-1] + [last_octet])
+        response_time = ping3.ping(current_ip)
+        if response_time is not False:
+            active_hosts.append(current_ip)
+            counter += 1
+        else:
+            counter += 1
+except KeyboardInterrupt:
+    print("You discontinued the search,this is what I found so far...")
+    for i in active_hosts:
+        print(f"{i} is active")
+        sys.exit(0)
 
-
+print("This are the active hosts:")
 for i in active_hosts:
     print(f"{i} is active")
 
